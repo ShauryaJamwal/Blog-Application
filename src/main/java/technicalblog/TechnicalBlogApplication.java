941295e17717c5ddf076f0c1ce22810495164471
ShauryaJamwal <shaurya.jamwal.9@gmail.com>
2020-04-27 21:33:56 +0530

    Created Home Controller

---
+++ src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -0,0 +1,15 @@
+package technicalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class TechnicalBlogApplication {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(TechnicalBlogApplication.class,args);
+    }
+
+}
---
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -0,0 +1,41 @@
+package technicalblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+
+    public String getAllPosts(Model model) {
+
+        ArrayList<Post> postsList = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setTitle("First Post");
+        post1.setBody("Post Body 1");
+        post1.setDate(new Date());
+
+        Post post2 = new Post();
+        post2.setTitle("Second Post");
+        post2.setBody("Post Body 2");
+        post2.setDate(new Date());
+
+        Post post3 = new Post();
+        post3.setTitle("Third Post");
+        post3.setBody("Post Body 3");
+        post3.setDate(new Date());
+
+        postsList.add(post1);
+        postsList.add(post2);
+        postsList.add(post3);
+
+        model.addAttribute("posts",postsList);
+        return "index";
+    }
+}
